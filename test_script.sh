caf37aac5f40a2cccbd6971661e6b7ce0ad3368a
elkaim_r <elkaim_r@epitech.eu>
2015-01-27 12:37:26 +0100

    script de test + ca compile

---
+++ test_script.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+## test_script.sh for script in /home/elkaim_r/projects/malloc
+## 
+## Made by raphael elkaim
+## Login   <elkaim_r@epitech.net>
+## 
+## Started on  Tue Jan 27 12:34:34 2015 raphael elkaim
+## Last update Tue Jan 27 12:35:09 2015 raphael elkaim
+##
+
+export LD_PRELOAD=./libmy_malloc.so
+./moul_malloc_stud
+unset LD_PRELOAD
