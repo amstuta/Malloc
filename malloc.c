3fd77c2f6a6b9d69eea0abbe92a89351de18b6e2
amstut_a <amstut_a@epitech.eu>
2015-01-27 11:00:36 +0100

    yo

--- malloc.c
+++ malloc.c
@@ -1,3 +1,13 @@
+/*
+** malloc.c for malloc in /home/amstut_a/malloc
+** 
+** Made by Arthur Amstutz
+** Login   <amstut_a@epitech.net>
+** 
+** Started on  Tue Jan 27 11:00:26 2015 Arthur Amstutz
+** Last update Tue Jan 27 11:00:28 2015 Arthur Amstutz
+*/
+
 void *malloc(size_t size)
 {
 }
