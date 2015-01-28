4e4bd174aa68a43e7a638406dd9c4451efcdebcc
elkaim_r <elkaim_r@epitech.eu>
2015-01-28 14:59:35 +0100

    je t'encule

---
+++ align.h
@@ -0,0 +1,16 @@
+/*
+** align.h for align in /home/elkaim_r/projects/malloc
+** 
+** Made by raphael elkaim
+** Login   <elkaim_r@epitech.net>
+** 
+** Started on  Wed Jan 28 11:08:17 2015 raphael elkaim
+** Last update Wed Jan 28 11:09:00 2015 raphael elkaim
+*/
+
+#ifndef ALIGN_H_
+# define ALIGN_H_
+
+void *align(void *ptr);
+
+#endif /* !ALIGN_H_ */
