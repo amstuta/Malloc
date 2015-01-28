d2673915cfcf26a5b075776cea1a0e8da08fd6c8
elkaim_r <elkaim_r@epitech.eu>
2015-01-28 15:55:54 +0100

    rah

--- test_script.sh
+++ test_script.sh
@@ -5,13 +5,13 @@
 ## Login   <elkaim_r@epitech.net>
 ## 
 ## Started on  Tue Jan 27 12:34:34 2015 raphael elkaim
-## Last update Wed Jan 28 15:51:30 2015 raphael elkaim
+## Last update Wed Jan 28 15:55:18 2015 raphael elkaim
 ##
 
 make
-export LD_PRELOAD=./libmy_malloc.so
 export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:./"
 gcc main.c -lmy_malloc -L./
+export LD_PRELOAD=./libmy_malloc.so
 ./a.out
 ./moul_malloc_stud
 unset LD_PRELOAD
